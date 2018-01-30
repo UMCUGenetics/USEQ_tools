0a5dc4eef9737ea19d66cd35cf33b374f2fc617a
sboymans <sboymans@hpcs04.op.umcutrecht.nl>
2018-01-30 15:40:56 +0100

    add bioinformatics protocol to config

--- epp_scripts/useq_route_artifacts_config.py
+++ epp_scripts/useq_route_artifacts_config.py
@@ -7,6 +7,6 @@ NEXT_STEPS = {
     'Illumina NextSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1226',
     'Illumina MiSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1229',
     'Illumina HiSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1232',
-    'USEQ - Library Pooling' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1222'
-
+    'USEQ - Library Pooling' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1222',
+    'USEQ - Post Sequencing' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1235'
 }
