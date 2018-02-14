fd460c643fcac981e0225dbcb4e5591c7aab3a18
sboymans <sboymans@hpcs04.op.umcutrecht.nl>
2018-02-14 13:03:38 +0100

    changed workflow uri's in config

--- epp_scripts/useq_route_artifacts_config.py
+++ epp_scripts/useq_route_artifacts_config.py
@@ -1,12 +1,10 @@
 NEXT_STEPS = {
-    'Truseq DNA nano (manual)' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1203',
-    'Truseq DNA nano (automatic)' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1149',
-    'Truseq RNA stranded ribo-zero' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1217',
-    'Truseq RNA stranded polyA (manual)' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1212',
-    'Truseq RNA stranded polyA (automatic)' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1207',
-    'Illumina NextSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1226',
-    'Illumina MiSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1229',
-    'Illumina HiSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1232',
-    'USEQ - Library Pooling' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1222',
-    'USEQ - Post Sequencing' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/511/stages/1235'
+    'Truseq DNA nano' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1349',
+    'Truseq RNA stranded ribo-zero' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1358',
+    'Truseq RNA stranded polyA' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1353',
+    'Illumina NextSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1367',
+    'Illumina MiSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1370',
+    'Illumina HiSeq' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1373',
+    'USEQ - Library Pooling' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1363',
+    'USEQ - Post Sequencing' : 'https://usf-lims-test.op.umcutrecht.nl/api/v2/configuration/workflows/514/stages/1376'
 }
