4a9875a76cac9ac2bd0b32a2f486c061e32992c2
Sander Boymans <sboymans@hpct02.op.umcutrecht.nl>
2023-01-25 09:03:37 +0100

    Bug fix in share template to account for 0 read samples

--- epp/useq_check_barcodes.py
+++ epp/useq_check_barcodes.py
@@ -12,20 +12,6 @@ def check_barcodes(lims, step_uri):
         if not artifact.reagent_labels:
             print(f'{sample.name} has no barcode')
             exit(-1)
-            # print (sample,artifact.reagent_labels[0])
-
-    # step = Step(lims, uri=step_uri)
-    # for io_map in step.details.input_output_maps:
-    #
-    #     artifact = None
-    #     next_stage = None
-    #
-    #     if io_map[1]['output-generation-type'] == 'PerInput':
-    #         artifact = io_map[1]['uri'] #output artifact
-    #         print(io_map[0]['uri'],artifact)
 
 def run(lims, step_uri):
 	check_barcodes(lims, step_uri)
-
-
-# https://usf-lims.umcutrecht.nl/api/v2/steps/24-409095
